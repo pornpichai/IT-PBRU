a0bf0111cadb99cf309d115e048a55cb2c2ff365
Pornpichai <boys-kon@hotmail.com>
2016-06-07 15:51:57 +0700

    Add value finish

--- app/src/main/java/pbru/example/pornpichai/itpbru/MainActivity.java
+++ app/src/main/java/pbru/example/pornpichai/itpbru/MainActivity.java
@@ -7,10 +7,21 @@ import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
+    //Explicit
+    private MyManage myManage;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        myManage = new MyManage(this);
+
+        //Test Add New User
+        //myManage.addNewUser("123", "name", "sur", "user", "pass");
+
+
     } //Main Method
 
     public void clickSingUpMain(View view) {
---
+++ app/src/main/java/pbru/example/pornpichai/itpbru/MyManage.java
@@ -0,0 +1,48 @@
+package pbru.example.pornpichai.itpbru;
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+
+/**
+ * Created by lab324-15 on 6/7/2016 AD.
+ */
+public class MyManage {
+
+    //Explicit
+    private  MyOpenHelper myOpenHelper;
+    private SQLiteDatabase sqLiteDatabase;
+
+    public static final String user_table = "userTABLE";
+    public static final String column_id = "_id";
+    public static final String column_name = "Name";
+    public static final String column_surname = "Surname";
+    public static final String column_user = "User";
+    public static final String column_password = "Password";
+
+    public MyManage(Context context) {
+
+
+        myOpenHelper = new MyOpenHelper(context);
+        sqLiteDatabase = myOpenHelper.getWritableDatabase();
+
+
+    } //Constructor
+
+    public long addNewUser(String strID,
+                           String strName,
+                           String strSurname,
+                           String strUser,
+                           String strPassword) {
+
+        ContentValues contentValues = new ContentValues();
+        contentValues.put(column_id, strID);
+        contentValues.put(column_name, strName);
+        contentValues.put(column_surname, strSurname);
+        contentValues.put(column_user, strUser);
+        contentValues.put(column_password, strPassword);
+
+        return sqLiteDatabase.insert(user_table, null, contentValues);
+    }
+
+} //Main Class
