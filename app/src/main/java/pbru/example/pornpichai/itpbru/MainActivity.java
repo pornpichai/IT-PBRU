f0723a6e1b1733600062f40718aeb0c134ed3570
Pornpichai <boys-kon@hotmail.com>
2016-06-07 15:59:54 +0700

    Delete database finish

--- app/src/main/java/pbru/example/pornpichai/itpbru/MainActivity.java
+++ app/src/main/java/pbru/example/pornpichai/itpbru/MainActivity.java
@@ -1,6 +1,7 @@
 package pbru.example.pornpichai.itpbru;
 
 import android.content.Intent;
+import android.database.sqlite.SQLiteDatabase;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -25,6 +26,17 @@ public class MainActivity extends AppCompatActivity {
         deleteAllSQLite();
 
     } //Main Method
+
+    private void deleteAllSQLite() {
+
+        SQLiteDatabase sqLiteDatabase = openOrCreateDatabase(MyOpenHelper.database_name,
+                MODE_PRIVATE, null);
+        sqLiteDatabase.delete(MyManage.user_table, null, null);
+
+
+    } //deleteAllSQLite
+
+
 
     public void clickSingUpMain(View view) {
         startActivity(new Intent(MainActivity.this, SignUpActivity.class));
